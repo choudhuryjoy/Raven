f811544a4bc5b483a2034cd6ab160be433c38bc0
Joy <joychoudhury2000@gmail.com>
2024-04-24 14:47:22 +0530

    FIX: signOutHelper

--- components/SignOutButton.tsx
+++ components/SignOutButton.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import { signout } from "@/lib/SignOutHelper";
+import { signOutHelper } from "@/lib/SignOutHelper";
 
 const SignOutButton = () => {
     return (
         <div>
             <button
-                onClick={async() => {
-                    await signout();
+                onClick={async () => {
+                    await signOutHelper();
                 }}
                 className="m-2 block mx-auto text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
             >
--- lib/signOutHelper.ts
+++ lib/signOutHelper.ts
@@ -2,6 +2,6 @@
 
 import { signOut } from "@/auth";
 
-export const signout = async () => {
+export const signOutHelper = async () => {
   await signOut();
 };
